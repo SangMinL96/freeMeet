90dfbd49d6938e3926272d3f4d02e99e73f342c5
sangmin.lee <hwon3794@gmail.com>
2022-09-26 16:24:35 +0900

    .

--- web/src/components/map/MapIndex.tsx
+++ web/src/components/map/MapIndex.tsx
@@ -2,45 +2,54 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 function MapIndex() {
+  const [myLocation, setMyLocation] = useState<{ La: number; Ma: number }>(
+    null
+  );
+  const [clickLocation, setClickLocation] = useState<{
+    La: number;
+    Ma: number;
+  }>(null);
   const [text, setText] = useState("");
 
   useEffect(() => {
-    var mapContainer = document.getElementById("map"), // 지도를 표시할 div
-      mapOption = {
-        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-        level: 3, // 지도의 확대 레벨
-      };
+    if (myLocation) {
+      var mapContainer = document.getElementById("map"), // 지도를 표시할 div
+        mapOption = {
+          center: new kakao.maps.LatLng(myLocation.Ma, myLocation.La), // 지도의 중심좌표
+          level: 3, // 지도의 확대 레벨
+        };
 
-    var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+      var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
-    // 지도를 클릭한 위치에 표출할 마커입니다
-    var marker = new kakao.maps.Marker({
-      // 지도 중심좌표에 마커를 생성합니다
-      position: map.getCenter(),
-    });
-    // 지도에 마커를 표시합니다
-    marker.setMap(map);
+      // 지도를 클릭한 위치에 표출할 마커입니다
+      var marker = new kakao.maps.Marker({
+        // 지도 중심좌표에 마커를 생성합니다
+        position: map.getCenter(),
+      });
+      // 지도에 마커를 표시합니다
+      marker.setMap(map);
 
-    // 지도에 클릭 이벤트를 등록합니다
-    // 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
-    kakao.maps.event.addListener(map, "click", function (mouseEvent) {
-      // 클릭한 위도, 경도 정보를 가져옵니다
-      var latlng = mouseEvent.latLng;
+      // 지도에 클릭 이벤트를 등록합니다
+      // 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
+      kakao.maps.event.addListener(map, "click", function (mouseEvent) {
+        // 클릭한 위도, 경도 정보를 가져옵니다
+        var latlng = mouseEvent.latLng;
 
-      // 마커 위치를 클릭한 위치로 옮깁니다
-      marker.setPosition(latlng);
-    });
+        // 마커 위치를 클릭한 위치로 옮깁니다
+        marker.setPosition(latlng);
+        setClickLocation(latlng);
+      });
+    }
   }, []);
   useEffect(() => {
     // RN에서 웹으로 데이터를 전송했을때 message이벤트가 실행됩니다.
     window.addEventListener("message", (e: any) => {
-      setText(e.data);
+      const { lat, long } = e.data;
+      setMyLocation({ Ma: lat, La: long });
     });
   }, []);
   return (
     <Container>
-      <div>왜왜왜왜ㅗ애ㅐ왜오왜ㅙ왱오오애</div>
-      <div>{text}</div>
       <button onClick={() => ReactNativeWebView.postMessage("야야야포스트")}>
         포스트
       </button>
